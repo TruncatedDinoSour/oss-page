732697c210a619fc47d5d331b27581e400a06784
Rstar284 <me@rstar.ml>
2022-05-08 14:27:14 +0400

    init

--- main.ts
+++ main.ts
@@ -7,6 +7,8 @@ import {
 
 const app = new Application();
 
+const database: [{ip: string, agent: string | undefined}] = [{ip: "", agent: "insert agent here"}];
+
 const router = new Router();
 router
   .get("/", (ctx: Context) => {
@@ -15,12 +17,25 @@ router
       return;
     }
     const split = agent.split(/[\s+|\/]/g)
+    let user: {ip: string, agent: string | undefined} | undefined = database.find(x => x.ip === ctx.request.ip)
+    if(!user) {
+      user = database.at(database.push({ip: ctx.request.ip, agent: split.at(-2)}))
+      return
+    }
+    let ua = user?.agent
     if(split.includes("Firefox")) {
+      ua = "Firefox"
       ctx.response.body = "Ooh you use firefox, or atleast appear to, (smh to those who changed their user-agent), anyway you deserve a cookie"
       ctx.response.headers.set("Refresh", "4;https://www.youtube.com/watch?v=dQw4w9WgXcQ")
     } else {
+      if(ua !== "Firefox") {
       ctx.response.body = "Please for all purposes and reasons, switch to Firefox for your daily usage. As you are not currently using Firefox, we cannot serve you this page, for this server only serves OSS consumers"
-      ctx.response.headers.set("Refresh", "4;https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+        ua = split.at(split.length - 2)
+      } else {
+        ctx.response.body = "You betrayer, how could you use firefox and then switch to another browser (or switch your user-agent), smh."
+        ua = split.at(split.length - 2)
+      }
+      user.agent = ua
     }
   })
 
